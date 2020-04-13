fa26ea1e7f094dfcdecec19e0d0a591b4f2b64fe
may051333 <55181052+may051333@users.noreply.github.com>
2020-04-13 21:08:23 +0800

    0413

--- Unity_ML_Football/Assets/Scripts/Robot.cs
+++ Unity_ML_Football/Assets/Scripts/Robot.cs
@@ -24,10 +24,10 @@ public class Robot : Agent
         rigBall.velocity = Vector3.zero;
         rigBall.angularVelocity = Vector3.zero;
 
-        Vector3 posRobot = new Vector3(Random.Range(-2f, 2f), 0.1f, Random.Range(-2f, 0f));
+        Vector3 posRobot = new Vector3(Random.Range(-1f, 1f), 0.1f, Random.Range(-1f, 0f));
         transform.position = posRobot;
 
-        Vector3 posBall = new Vector3(Random.Range(-2f, 2f), 0.1f, Random.Range(1f, 2f));
+        Vector3 posBall = new Vector3(Random.Range(-0.5f, 0.5f), 0.1f, Random.Range(1f, 1.5f));
         rigBall.position = posBall;
 
         Ball.complete = false;
@@ -54,7 +54,7 @@ public class Robot : Agent
             EndEpisode();
         }
 
-        if (transform.position.y < 0 || rigBall.position.y < 0) ;
+        if (transform.position.y < 0 || rigBall.position.y < 0) 
         {
             SetReward(-1);
             EndEpisode();
